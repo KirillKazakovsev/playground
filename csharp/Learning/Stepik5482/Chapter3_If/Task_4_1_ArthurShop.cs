3857f1c014a1a296d8a4450f523dd76050ac0417
KirillKazakovsev <kiryxaa@gmail.com>
2022-01-07 21:42:22 +0300

    Решение

--- csharp/Learning/Stepik5482/Chapter3_If/Task_4_1_ArthurShop.cs
+++ csharp/Learning/Stepik5482/Chapter3_If/Task_4_1_ArthurShop.cs
@@ -10,7 +10,16 @@ namespace Stepik5482.Chapter3_If
     {
         public static void Do()
         {
-            Console.WriteLine("Task_4_1_ArthurShop");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int s1 = a + a + b + b;
+            int s2 = a + c + b;
+            int s3 = a + c + c + a;
+            int s4 = b + c + c + b;
+            int m1 = Math.Min(s1, s2);
+            int m2 = Math.Min(s3, s4);
+            Console.WriteLine(Math.Min(m1, m2));
         }
     }
 }
