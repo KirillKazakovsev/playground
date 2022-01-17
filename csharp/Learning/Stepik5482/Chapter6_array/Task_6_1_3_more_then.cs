54d71c491a9145644fbf08defbf8ccee85a9a8a9
KirillKazakovsev <kiryxaa@gmail.com>
2022-01-17 19:48:21 +0300

    Commit

--- csharp/Learning/Stepik5482/Chapter6_array/Task_6_1_3_more_then.cs
+++ csharp/Learning/Stepik5482/Chapter6_array/Task_6_1_3_more_then.cs
@@ -10,7 +10,18 @@ namespace Stepik5482.Chapter6_array
     {
         public static void Do()
         {
-
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            string[] s = Console.ReadLine().Split();
+            int[] b = new int[n];
+            b[0] = Convert.ToInt32(s[0]);
+            for (int i = 1; i < s.Length; i++)
+            {
+                b[i] = Convert.ToInt32(s[i]);
+                if (b[i] > b[i - 1])
+                    a++;
+            }
+            Console.Write(a);
         }
     }
 }
