b3a376db6e97d15306e6e73798afcc9d29afd22c
KirillKazakovsev <kiryxaa@gmail.com>
2022-01-26 10:00:51 +0300

    Commit

---
+++ csharp/Learning/Stepik5482/Chapter6_array/Task_6_1_4_transposition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik5482.Chapter6_array
+{
+    internal class Task_6_1_4_transposition
+    {
+
+        public static void Do()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] s = Console.ReadLine().Split();
+            for (int i = 1; i < n; i += 2)
+                Console.Write(s[i] + " " + s[i - 1] + " ");
+            if (n % 2 != 0)
+                Console.Write(s[n - 1]);
+        }
+    }
+}
