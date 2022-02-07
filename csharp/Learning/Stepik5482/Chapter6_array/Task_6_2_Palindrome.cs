41c96742c46e00a2f14d4c6d99715c476bac4aae
KirillKazakovsev <kiryxaa@gmail.com>
2022-02-07 15:43:28 +0300

    Палиндром

---
+++ csharp/Learning/Stepik5482/Chapter6_array/Task_6_2_Palindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik5482.Chapter6_array
+{
+    internal class Task_6_2_Palindrome
+    {
+        public static void Do()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());               
+            string[] s = Console.ReadLine().Split();
+            int a = 0;
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i - 1])
+                {
+                    Console.Write("NO");
+                    a++;
+                    break;
+                }
+            }
+            if (a == 0)
+                Console.Write("YES");
+        }
+    }
+}
--- csharp/Learning/Stepik5482/Program.cs
+++ csharp/Learning/Stepik5482/Program.cs
@@ -13,7 +13,7 @@ namespace Stepik5482
     {
         static void Main(string[] args)
         {
-            Task_6_2_FistMin_LastMax.Do();
+            Task_6_2_Palindrome.Do();
             
             Console.ReadLine();            
         }
