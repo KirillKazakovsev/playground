f782b5b566209e503928a43dce084289a0177fdc
Kirill <kiryxaa@gmail.com>
2024-07-02 22:40:07 +0300

    ТЕСТ

---
+++ csharp/Learning/Stepik5482/Chapter6_array/Task_6_6_even_and_quantity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stepik5482.Chapter6_array
+{
+    internal class Task_6_6_even_and_quantity
+    {
+        public static void Do()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[n, n];
+            for (int i = n; i > 0; i--)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j + 1)
+                    {
+                        Console.Write(1 + " ");
+                    }
+                    else if (i < j + 1)
+                    {
+                        Console.Write(2 + " ");
+                    }
+                    else if (i > j)
+                    {
+                        Console.Write(0 + " ");
+                    }
+                }
+                Console.WriteLine();
+                int n = Convert.ToInt32(Console.ReadLine()); // сколько чисел будут вводиться на следующей строке
+                string s = Console.ReadLine(); // сохранить все введенную строку
+                string[] ss = s.Split(' '); // разделить введенную строку по пробелам и сохранить в массив строкового типа
+
+                int[] numbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    int number = Convert.ToInt32(ss[i]);
+                    numbers[i] = number;
+                    // Console.WriteLine(numbers[i]+" ");
+                }
+
+                //  считаем:
+                int cnt = 0;
+                string res = "";
+                for (int i = 0; i < n; i++)
+                {
+                    if (numbers[i] % 2 != 0) //если нечетное- пропускаем
+                    {
+                        continue;
+                    }
+                }
+    }
+}
---
+++ csharp/Learning/TEST/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int num1 = number / 100000;
+            int num2 = (number / 10000) % 10;
+            int num3 = (number / 1000) % 10;
+            int num4 = (number / 100) % 10;
+            int num5 = (number / 10) % 10;
+            int num6 = number % 10;
+            if ((num1 + num2 + num3) == (num4 + num5 + num6))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
