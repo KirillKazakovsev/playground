39bd182510a11c2d079d88e38d12d831607bf2aa
Kirill <kiryxaa@gmail.com>
2024-07-02 23:08:00 +0300

    Тест2

--- csharp/Learning/TEST/Program.cs
+++ csharp/Learning/TEST/Program.cs
@@ -4,14 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int number = Convert.ToInt32(Console.ReadLine());
-            int num1 = number / 100000;
-            int num2 = (number / 10000) % 10;
-            int num3 = (number / 1000) % 10;
-            int num4 = (number / 100) % 10;
-            int num5 = (number / 10) % 10;
-            int num6 = number % 10;
-            if ((num1 + num2 + num3) == (num4 + num5 + num6))
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
             {
                 Console.WriteLine("YES");
             }
@@ -19,6 +16,7 @@
             {
                 Console.WriteLine("NO");
             }
+
         }
     }
 }
